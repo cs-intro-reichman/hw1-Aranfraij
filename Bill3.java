c711e6d2b45d2a7f8ed64227f5ff811b5be5734d
Aranfraij <aran.fraij@post.runi.ac.il>
2024-11-14 12:12:31 +0200

    Add files via upload

--- Bill3.java
+++ Bill3.java
@@ -1,14 +1,17 @@
-// Splits a restaurant bill evenly among three diners.
+
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+
+
+    public static void main(String[] args) {
+    
+        String name1 = args[0];
         String name2 = args[1] ;
         String name3 = args[2] ;
-        int d;
-        d = Integer.parseInt(args[3]);
+        double d;
+        d = Double.parseDouble(args[3]);
         double e = Math.ceil( d /3);
-        System.out.println(name3+ ", "+ name2+ " and "+name1+" "+ d);
-        System.out.println("dear " +name3 + ", " +name2+", and "+ name1+ ": pay " + e + " shekels each");   
-	}
+        System.out.println("Dear "+name3+ ", "+name2+", and "+name1+": pay "+e);
+        //System.out.println(name3+ ", "+ name2+ " and "+name1+" "+ d);
+        //System.out.println("dear " +name3 + ", " +name2+", and "+ name1+ ": pay " + e + " shekels each");
+    }
 }
